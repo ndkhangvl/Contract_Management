4ac9160a81ddb376e9035a8f638d8f4a0b1c19c0
ndkhangvl <nguyenduykhang410@gmail.com>
2023-05-16 10:21:07 +0700

    Update Login form

---
+++ resources/views/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container-fluid vh-100" style="margin-top:300px">
+        <div class="" style="margin-top:200px">
+            <div class="rounded d-flex justify-content-center">
+                <div class="col-md-4 col-sm-12 shadow-lg p-5 bg-light">
+                    <div class="text-center">
+                        <h3 class="text-primary">Quản Lý Hợp Đồng</h3>
+                    </div>
+                    <form action="">
+                        <div class="p-4">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text bg-primary"><i
+                                        class="bi bi-person-plus-fill text-white"></i></span>
+                                <input type="text" class="form-control" placeholder="Tên tài khoản">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text bg-primary"><i
+                                        class="bi bi-key-fill text-white"></i></span>
+                                <input type="password" class="form-control" placeholder="Mật khẩu">
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Ghi nhớ tôi
+                                </label>
+                            </div>
+                            <button class="btn btn-primary text-center mt-2 mx-auto" type="submit">
+                                Đăng nhập
+                            </button>
+                            <p class="text-center text-primary">Quên mật khẩu?</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
